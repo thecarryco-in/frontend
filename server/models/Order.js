78bf8ae8a304421de07eaccb2e4ae1c0dc5381ab
thecarryco-in <thecarryco.in@gmail.com>
2025-06-23 21:18:01 +0530

    Update Order.js

--- server/models/Order.js
+++ server/models/Order.js
@@ -98,7 +98,7 @@ orderSchema.pre('save', async function(next) {
 
 // Index for better performance
 orderSchema.index({ user: 1, createdAt: -1 });
-orderSchema.index({ orderNumber: 1 });
+// orderSchema.index({ orderNumber: 1 });
 orderSchema.index({ status: 1 });
 
 export default mongoose.model('Order', orderSchema);
