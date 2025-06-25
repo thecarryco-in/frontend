b096f6d7ff3b177431613f316a0cc01c506367a6
thecarryco-in <thecarryco.in@gmail.com>
2025-06-25 22:20:07 +0530

    Create rateLimiters.js

---
+++ server/middleware/rateLimiters.js
@@ -0,0 +1,25 @@
+import rateLimit from 'express-rate-limit';
+
+export const authLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 5,
+  message: 'Too many requests from this IP, please try again later.'
+});
+
+export const contactLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 3,
+  message: 'Too many contact requests, please try again later.'
+});
+
+export const reviewLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 5,
+  message: 'Too many reviews from this IP, please try again later.'
+});
+
+export const apiLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100,
+  message: 'Too many requests from this IP, please try again later.'
+});
