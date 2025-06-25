df8aca4123df34b364e758ebd15794ef4ccbb7d8
thecarryco-in <thecarryco.in@gmail.com>
2025-06-25 21:15:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -102,7 +102,7 @@ app.use('*', (req, res) => {
 // Rate limiting middleware
 export const authLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 5,
+  max: 10,
   message: 'Too many requests from this IP, please try again later.'
 });
 
