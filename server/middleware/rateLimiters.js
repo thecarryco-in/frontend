09f6b3fef1a6a0e782c4599b95aad15b29391f4c
thecarryco-in <thecarryco.in@gmail.com>
2025-06-25 22:34:16 +0530

    Update rateLimiters.js

--- server/middleware/rateLimiters.js
+++ server/middleware/rateLimiters.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const authLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 5,
+  max: 10,
   message: 'Too many requests from this IP, please try again later.'
 });
 
@@ -20,6 +20,6 @@ export const reviewLimiter = rateLimit({
 
 export const apiLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 120,
+  max: 150,
   message: 'Too many requests from this IP, please try again later.'
 });
